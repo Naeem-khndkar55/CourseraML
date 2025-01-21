c4f04eebc6437f94ae9072779a4830bf72e7e237
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-21 21:44:59 +0600

    data type code done

---
+++ DataTypes.py
@@ -0,0 +1,11 @@
+print(type("hello"))  #chek the type of data using type function
+print('abc') #single qouted String
+print("hi guys")# double qouted string
+print('"abcd"')# double qouted string in the single qoutetion it print string with double qoute
+print("'pqr'") # will print single qoute with string
+print('''naeem''') # triple single qoute will display the string
+print("""khandakar""")#triple double qoute will display string
+print("naeem's life")#for using single qoute as string
+print(' mim says "hi!"') #single qoute to use double qoute as string
+print(1,2,3,4,5,6,100)#can display multiple value in single print function
+print(1,2,"three",'four',5) #multiple type value can be dispalayed
