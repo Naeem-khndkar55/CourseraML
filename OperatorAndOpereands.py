d032adacc8defcf0d2c3949b3c26dcf3668a06ae
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-21 20:12:14 +0600

    operator and operands are done

---
+++ OperatorAndOpereands.py
@@ -0,0 +1,8 @@
+print(100+200) # addition operator(+)
+print(100-20)#subtraction operator
+print(3*5) #multiplication operator
+print(4/2) # didvition operator
+print(7%2) # modulo operator that gives reminder
+print(2**3)#exponentiation operator(**) work like power
+print(16 - 2 * 5 // 3 + 1) 
+print(10//3)
