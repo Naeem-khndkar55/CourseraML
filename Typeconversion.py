bc9dc0b722e0507b705d4d0a745d9d585788da8e
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-23 20:57:00 +0600

    Type conversion

---
+++ Typeconversion.py
@@ -0,0 +1,10 @@
+
+r=5+10
+print("The value is" +str(r)) #if you want to concatinate the intiger with strig then we need to convert the type 
+
+a=17.72
+b=str(a)
+print(b) 
+c=float(b)
+print(c)
+print(type(c)) #floart type
