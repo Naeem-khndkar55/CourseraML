563a406938c3c042e4e5a555749e8f4ec6c508a4
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-08 22:48:00 +0600

    string tupple list basics

---
+++ Sequence.py
@@ -0,0 +1,10 @@
+s= "hi guys" #string
+print(s)
+print(type(s))
+
+myList=["naeem",4,"sady",1.35] #list
+print(myList) 
+print(type(myList))
+myTupple = ("mim", 0 , 20, 2.3,"dim") #tupple
+print(myTupple)
+print(type(myTupple))
