5cd75b642c2caccd4d2548ec3dafc1001f4017fa
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-11 10:51:54 +0600

    length function dne

--- Sequence.py
+++ Sequence.py
@@ -8,3 +8,11 @@ print(type(myList))
 myTupple = ("mim", 0 , 20, 2.3,"dim") #tupple
 print(myTupple)
 print(type(myTupple))
+print(len(s)) ##lenght of a sequence
+print(s[-1]) # reverse index 1 based;
+print(s[-7])# negtive index work on list and tuppple also
+#lenth function
+l="lengthofsequence"
+print(len(l))
+midchar=l[len(l)//2]
+print(midchar)
