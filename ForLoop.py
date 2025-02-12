2b5da8c7a8ec0c0bfc29d1d297bec7c627eed094
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-12 21:05:20 +0600

    For loop

---
+++ ForLoop.py
@@ -0,0 +1,24 @@
+for num in [1,2,3,4,5,6,7]:
+   print(num)
+list =['a','b','c','d','e']
+for li in list:
+   print(li)
+str="hi i am naeem"
+for ch in str:
+   print(ch)      
+
+
+##turtle
+import turtle            # set up alex
+wn = turtle.Screen()
+alex = turtle.Turtle()  
+import turtle            # set up alex
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+for aColor in ["yellow", "red", "purple", "blue"]:
+    alex.color(aColor)
+    alex.forward(50)
+    alex.left(90)
+
+wn.exitonclick()
