a4aee18cfa48b98f6204d043d00ecd5bf81092b2
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-13 21:29:40 +0600

    Logical operator

---
+++ LogicalOperator.py
@@ -0,0 +1,10 @@
+#and
+#or
+#not
+#in
+#not in  
+print('a'in "apple")
+print('d' not in "apple")
+print("a" in ["a", "b", "c", "d"])
+print(9 in [3, 2, 9, 10, 9.0])
+print('wow' not in ['gee wiz', 'gosh golly', 'wow', 'amazing'])
