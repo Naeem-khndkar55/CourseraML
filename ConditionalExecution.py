1f93bc02e128ccbd756f8358ec4174e74c9c77a8
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-14 22:34:49 +0600

    if else condition

---
+++ ConditionalExecution.py
@@ -0,0 +1,19 @@
+#if
+#elif
+#else
+s=15
+if(s%2==0):
+    print("s is even")
+else:
+    print("s is Odd")
+
+
+
+#with elif
+x=10
+if(x%2==0):
+    print("s is even")
+elif(x==0):
+    print("s is zero")    
+else:
+    print("s is Odd")
